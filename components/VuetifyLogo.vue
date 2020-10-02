ae316d3c963900dceec94224b019b4dc37427327
Nalin2000 <nalinsaytongchang@gmail.com>
2020-10-02 23:09:50 +0700

    B2

--- components/VuetifyLogo.vue
+++ components/VuetifyLogo.vue
@@ -1,14 +1,14 @@
 <template>
   <v-img
-    src="https://juststickers.in/wp-content/uploads/2019/10/space-saturn.png"
+    src="https://media.giphy.com/media/5h9hhfCzYF9x9eF1gf/giphy.gif"
     class="VuetifyLogo"
   />
 </template>
 
 <style>
 .VuetifyLogo {
-  height: 200px;
-  width: 200px;
+  height: 250px;
+  width: 250px;
   transform: rotateY(560deg);
   animation: turn 3.5s ease-out forwards 1s;
 }
